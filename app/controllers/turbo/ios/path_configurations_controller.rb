9fb6419ef17a8d12598c9466ad46850b88a14a10
William Kennedy <williamkennedyjnr@gmail.com>
2023-05-02 14:38:42 +0100

    phase 1

--- app/controllers/turbo/ios/path_configurations_controller.rb
+++ app/controllers/turbo/ios/path_configurations_controller.rb
@@ -2,16 +2,17 @@ module Turbo
   module Ios
     class PathConfigurationsController < ApplicationController
       def show
-        render json: {
-          rules: [
-            {
-              patterns: ["/new$", "/edit$"],
-              properties: {
-                context: "modal"
-              }
-            }
-          ]
-        }
+        render json: {}
+        # render json: {
+        #   rules: [
+        #     {
+        #       patterns: ["/new$", "/edit$"],
+        #       properties: {
+        #         context: "modal"
+        #       }
+        #     }
+        #   ]
+        # }
       end
     end
   end
